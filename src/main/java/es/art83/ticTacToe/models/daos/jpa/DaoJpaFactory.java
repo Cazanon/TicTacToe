eead807ebc0fbc40c3eec335a22b772753cdd4c9
miw-upm <miw@eui.upm.es>
2015-02-11 23:24:09 +0100

    jpa createTables

--- src/main/java/es/art83/ticTacToe/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/es/art83/ticTacToe/models/daos/jpa/DaoJpaFactory.java
@@ -1,9 +1,13 @@
 package es.art83.ticTacToe.models.daos.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.logging.log4j.LogManager;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 import es.art83.ticTacToe.models.daos.PieceDao;
 import es.art83.ticTacToe.models.daos.SessionDao;
@@ -14,15 +18,25 @@ import es.art83.ticTacToe.models.daos.PlayerDao;
 public class DaoJpaFactory extends DaoFactory {
     private static final String PERSISTENCE_UNIT = "tictactoe";
 
-    private static EntityManagerFactory entityManagerFactory = Persistence
-            .createEntityManagerFactory(PERSISTENCE_UNIT);
+    private static EntityManagerFactory entityManagerFactory; 
 
     public DaoJpaFactory() {
-        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
     }
 
     public static EntityManagerFactory getEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+        }
         return entityManagerFactory;
+    }
+
+    public static void dropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory("BBDD", properties);
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
     }
 
     @Override
